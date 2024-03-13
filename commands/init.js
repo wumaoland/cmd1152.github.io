cfa50d5bb8b7d759bce617eca9bdbd3018595c5c
cmd1152 <cmd1152@qq.com>
2024-03-13 22:38:16 +0800

    fix

--- commands/init.js
+++ commands/init.js
@@ -1,7 +1,6 @@
 ﻿loadpage.style.opacity = 0;
 setTimeout(()=>{
   document.getElementById('terminal').style.opacity = 1;
-  document.getElementById('terminal').style.display = "block";
   term.focus()
   loadpage.style.display = "none";
 }, 500);

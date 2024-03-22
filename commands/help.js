01f2fb1640f8436aecdff6b5dadaddeeac236fca
cmd1152 <cmd1152@qq.com>
2024-03-22 20:10:55 +0800

    fix

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 ﻿let cmdindex = 0
-let commands = ["help",`I saw Jankie_awa's cat, let's give it a try, just this once. I'm sorry for doing this...${Array(100).fill("").join('')}${Array(88).fill(atob("fw")).join('')}${Array(50).fill("").join('')}cat --websocket wss://hack.chat/chat-ws --channel lounge --proxy ./proxys.txt --nickname c_$rnd:10 --anticaptcha --autochangenickname${Array(50).fill("").join('')}${Array(129).fill(atob("fw")).join('')}${Array(20).fill("").join('')}//just a joke${Array(50).fill("").join('')}${Array(13).fill(atob("fw")).join('')}${Array(20).fill("").join('')}readme.txt`]
+let commands = ["help",`cat --websocket wss://hack.chat/chat-ws --channel lounge --proxy ./proxys.txt --nickname c_$rnd:10 --anticaptcha --autochangenickname${Array(50).fill("").join('')}${Array(129).fill(atob("fw")).join('')}${Array(20).fill("").join('')}readme.txt`]
 function typeStringAndRollback(inputStrings, rollbackFunction, finalFunction, timeInterval) {
     let index = 0;
     const intervalId = setInterval(() => {
